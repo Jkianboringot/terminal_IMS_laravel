9365fb586dbb6e5fc7f55d3ad3ccf76b5b0ebbf6
Jkianboringot <jkianboringot@gmail.com>
2025-06-24 00:37:00 +0800

    still dont understand

--- app/Livewire/Admin/ProductCategories/Index.php
+++ app/Livewire/Admin/ProductCategories/Index.php
@@ -2,12 +2,16 @@
 
 namespace App\Livewire\Admin\ProductCategories;
 
+use App\Models\ProductCategory;
 use Livewire\Component;
 
 class Index extends Component
 {
+ 
     public function render()
     {
-        return view('livewire.admin.product-categories.index');
+        return view('livewire.admin.product-categories.index', [
+            'productCategories' => ProductCategory::all()
+        ]);
     }
 }
