269f50736474ba9fcdd91c9986a67b84b9e10e6b
Jkianboringot <slyrics443@gmail.com>
2025-09-24 17:46:17 -0700

    created unsuccessfull transac migration and migration list for it

---
+++ database/migrations/2025_09_24_040415_create_unsuccessful_transactions_to_list_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('unsuccessful_transactions_to_list', function (Blueprint $table) {
+         $table->foreignId('product_id')->constrained();
+            $table->foreignId('add_product_id')->constrained();
+            $table->primary(['product_id','add_product_id']);
+            $table->decimal('quantity',10,2)->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('unsuccessful_transactions_to_list');
+    }
+};
